8305968727e6d59915e2c41935336bc3b4b55090
Shiva <isha@abcd.com>
2020-10-16 09:18:27 +0530

    fb001-working version

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args){
         String name = "rsc";
+        String changes = "Test123";
         System.out.println("Hello World application" + name);
-    }
+        System.out.println("new changes .." + changes);
 
 }
